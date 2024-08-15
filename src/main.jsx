63d0d799c34e858452d1a0c36a6250933671c1dc
citrus <1916826474@qq.com>
2024-08-15 11:33:58 +0800

    🐳 chore(安装依赖，添加配置，修改基础信息):

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App'
+import './assets/index.css'
+import 'normalize.css'    // 重置样式
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {  //配置@别名
+        "@": path.resolve(__dirname, './src')
+    },
+    extensions: ['.jsx']  //配置文件后缀
+  },
+
+  css: {
+    postcss: {
+      plugins: [
+        // css样式自动添加兼容前缀
+        autoprefixer()
+      ]
+    }
+  },
+
+  // 运行后自动打开网页
+  server: {
+    open: true
+  }
 })
